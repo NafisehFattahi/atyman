35dc857c11aa7fd6b034c00d56de4c7d560b6816
NafisehFattahi <nafiseh23fattahi@gmail.com>
2024-09-08 17:21:30 +0330

    18th

--- run-server.js
+++ run-server.js
@@ -5,7 +5,7 @@ const app = express();
 // Change this based on your astro.config.mjs, `base` option.
 // They should match. The default value is "/".
 // const base = ".";
-app.use(express.static("./public"));
+app.use(base, express.static("dist/client/"));
 app.use(ssrHandler);
 
 app.listen(3000); // this should be the same port that is being reverse proxied indicated in the nginx config file
