e0a6c50c3f0a326e0701bbaee5af63184784be8d
NafisehFattahi <nafiseh23fattahi@gmail.com>
2024-09-08 17:42:13 +0330

    20th

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: "https://atyman.com",
+  base: "/",
   integrations: [tailwind()],
   output: "server",
   adapter: node({
